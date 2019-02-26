6540992acefe0294e1963c52b6505254c7d08970
MgLa19 <mglaaa6@gmail.com>
2019-02-26 22:39:30 +0630

    assign

---
+++ project/new assignment/Controllers/PartiesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using new_assignment.Models;
+
+namespace new_assignment.Controllers
+{
+    public class PartiesController : Controller
+    {
+        private AssignmentEntities db = new AssignmentEntities();
+
+        // GET: Parties
+        public ActionResult Index()
+        {
+            var categories = db.Parties.Include(x => x.Menus).ToList();
+            return View(categories);
+        }
+
+        // GET: Parties/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Party party = db.Parties.Find(id);
+            if (party == null)
+            {
+                return HttpNotFound();
+            }
+            return View(party);
+        }
+
+        // GET: Parties/Create
+        public ActionResult Create(int price)
+        {
+                return View();     
+        }
+
+        // POST: Parties/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,PartyName,Decoration")] Party party)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parties.Add(party);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(party);
+        }
+
+        // GET: Parties/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Party party = db.Parties.Find(id);
+            if (party == null)
+            {
+                return HttpNotFound();
+            }
+            return View(party);
+        }
+
+        // POST: Parties/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,PartyName,Decoration")] Party party)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(party).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(party);
+        }
+
+        // GET: Parties/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Party party = db.Parties.Find(id);
+            if (party == null)
+            {
+                return HttpNotFound();
+            }
+            return View(party);
+        }
+
+        // POST: Parties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Party party = db.Parties.Find(id);
+            db.Parties.Remove(party);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ project/new assignment/Controllers/finalordersController.cs
@@ -0,0 +1,144 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using new_assignment.Models;
+using Microsoft.AspNet.Identity;
+
+namespace new_assignment.Controllers
+{
+    public class finalordersController : Controller
+    {
+        private AssignmentEntities db = new AssignmentEntities();
+
+        // GET: finalorders
+        public ActionResult Index()
+        {
+            return View(db.finalorders.ToList());
+        }
+
+        public ActionResult SendEmail() {  
+    return View();  
+}  
+
+
+        // GET: finalorders/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            finalorder finalorder = db.finalorders.Find(id);
+            if (finalorder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(finalorder);
+        }
+
+        // GET: finalorders/Create
+        public ActionResult Create(decimal price)
+        {
+            string guest = Request.QueryString["guest"];
+            ViewBag.price = price;
+            ViewBag.guest = int.Parse(guest);
+            return View();
+        }
+
+        // POST: finalorders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Phone_number,Email,Address,Party_date,Party_time")] finalorder finalorder)
+        {
+            if (User.Identity.GetUserId()==null)
+            {
+                ModelState.AddModelError("Error", "Please...Login to continue.");
+                ViewBag.Name = new SelectList(db.finalorders, "Name", "Name");
+                return RedirectToAction("Login","Account");
+            }
+            else
+            {
+                finalorder.id = Guid.NewGuid();
+                db.finalorders.Add(finalorder);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(finalorder);
+        }
+
+        // GET: finalorders/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            finalorder finalorder = db.finalorders.Find(id);
+            if (finalorder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(finalorder);
+        }
+
+        // POST: finalorders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Phone_number,Email,Address,Party_date,Party_time")] finalorder finalorder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(finalorder).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(finalorder);
+        }
+
+        // GET: finalorders/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            finalorder finalorder = db.finalorders.Find(id);
+            if (finalorder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(finalorder);
+        }
+
+        // POST: finalorders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            finalorder finalorder = db.finalorders.Find(id);
+            db.finalorders.Remove(finalorder);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
